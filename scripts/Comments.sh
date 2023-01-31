24261a246ffe221b384b445a7fcf61e523becb13
alfmorais <alfredneto@hotmail.com>
2023-01-31 19:09:49 -0300

    inicia o capitulo de scripts

---
+++ scripts/Comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##############################################################################
+#
+# Comments.sh - Script de Exemplo do Curso Shell Script Profissional
+# 
+# Autor: Alfredo de Morais Neto (alfredneto@hotmail.com)
+# Data de Criação: 31/01/2023
+#
+# Descrição: Imprime na tela uma mensagem
+# 
+# Exemplo de uso: ./Comments.sh
+# 
+##############################################################################
+echo "Comentários em Script Shell"
